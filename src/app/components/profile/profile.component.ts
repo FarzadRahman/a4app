e675f668f75ff37059bf5d00e4134b660897e3d4
Farzad <farzadrahman59@gmail.com>
2018-06-30 00:53:29 +0600

    Farzad

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  image: File;
+  carouselImage:any= [
+      {alt: 'Los Angeles', image: 'https://www.nathab.com/uploaded-files/carousels/TRIPS/Tanzania-Migration-Photo/Carousel-Tanzania-Migration-Photo-Safari-100.jpg'},
+      {alt: 'Chicago', image: 'http://wowslider.com/sliders/demo-93/data1/images/landscape.jpg'},
+      {alt: 'New York', image: 'http://cssslider.com/sliders/demo-28/data1/images/scooter593155_1280.jpg'},
+  ];
   constructor() { }
 
   ngOnInit() {
